726d7b078421d61176da7cc7288cb9f2e6f12ff3
camilo <camilomontoya2@gmail.com>
2019-11-27 10:51:18 -0500

    HttpClientModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { MDBBootstrapModule, NavbarModule } from "angular-bootstrap-md";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MDBBootstrapModule.forRoot(), NavbarModule],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    NavbarModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
