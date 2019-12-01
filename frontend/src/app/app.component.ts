dbadad04236f4074cc485fdfcc7b95c9eb768cfb
camilo <camilomontoya2@gmail.com>
2019-12-01 11:11:00 -0500

    navbar y cambio de vista

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,11 @@ import { NavbarComponent } from "angular-bootstrap-md";
   styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
+  public vista = "usuarios";
   @ViewChild("navbar", { static: true }) navbar: NavbarComponent;
   title = "frontend";
+
+  cambiarVista(nuevaVista: string): void {
+    this.vista = nuevaVista;
+  }
 }
