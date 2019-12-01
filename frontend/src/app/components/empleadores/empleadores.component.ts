570ef5e9af0a40e494d1ff4f2259f4ded98ed6a7
camilo <camilomontoya2@gmail.com>
2019-12-01 10:43:10 -0500

    actualizo empleadores component html

---
+++ frontend/src/app/components/empleadores/empleadores.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from "@angular/core";
+import { UsuariosService } from "../../services/usuarios.service";
+import { FormControl } from "@angular/forms";
+
+@Component({
+  selector: "usuarios",
+  templateUrl: "./usuarios.component.html"
+})
+export class UsuariosComponent implements OnInit {
+  constructor(private usuariosService: UsuariosService) {}
+
+  public usuarios = [];
+  public headElements = ["id", "nombre", "acciones"];
+
+  public nombreInput = new FormControl();
+
+  ngOnInit(): void {
+    this.getUsuarios();
+  }
+  getUsuarios(): void {
+    this.usuariosService.getUsuarios().subscribe(data => {
+      console.log({ data });
+      this.usuarios = data;
+    });
+  }
+
+  deleteUsuario(indice: number): void {
+    this.usuariosService.deleteUsuario(indice).subscribe(data => {
+      console.log({ data });
+      this.getUsuarios();
+    });
+  }
+
+  createUsuario(): void {
+    const nuevoUsuario: any = {
+      nombre: this.nombreInput.value || ""
+    };
+    console.log("click createUsuario === ", { nuevoUsuario });
+    this.usuariosService.createUsuario(nuevoUsuario).subscribe(data => {
+      console.log({ data });
+      this.getUsuarios();
+    });
+  }
+}
